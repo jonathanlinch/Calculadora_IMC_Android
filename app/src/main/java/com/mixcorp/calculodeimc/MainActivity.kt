4e18d66ba81e74e871b1af4ec58e5676178f588e
Jamela <jonathanlinch7@gmail.com>
2022-05-28 12:13:14 -0300

    lógica do cálculo de imc criação da segunda activity

--- app/src/main/java/com/mixcorp/calculodeimc/MainActivity.kt
+++ app/src/main/java/com/mixcorp/calculodeimc/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.mixcorp.calculodeimc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("NAME_SHADOWING")
+const val valorImc = "com.mixcorp.calculodeimc.IMC"
+@Suppress("NAME_SHADOWING")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
+
     }
 
-   private fun setListeners(){
-        
+    private fun setListeners() {
+        val pesoEdt = findViewById<EditText>(R.id.pesoEdt)
+        val alturaEdt = findViewById<EditText>(R.id.alturaEdt)
+        val idadeEdt = findViewById<EditText>(R.id.idadeEdt)
+        val calcularBtn = findViewById<Button>(R.id.btnCalcular)
+
+        calcularBtn.setOnClickListener{
+           calcularIMC(pesoEdt.text.toString(), alturaEdt.text.toString(), idadeEdt.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String, idade: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        val idade = idade.toIntOrNull()
+
+            if (peso != null && altura != null && idade != null) {
+                val imc = peso / (altura * altura)
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra(valorImc,imc)
+                }
+                startActivity(intent)
+        }
+
+
     }
 }
+
