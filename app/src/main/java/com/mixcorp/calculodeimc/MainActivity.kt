9e18043ff82c990b0a4b92d2cfaa457578019c56
Jamela <jonathanlinch7@gmail.com>
2022-06-18 23:13:19 -0300

    Adicionado os anuncios interstitial e conexão admob

--- app/src/main/java/com/mixcorp/calculodeimc/MainActivity.kt
+++ app/src/main/java/com/mixcorp/calculodeimc/MainActivity.kt
@@ -4,25 +4,33 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.mixcorp.calculodeimc.R.color.red
 import com.mixcorp.calculodeimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mInterstitialAd: InterstitialAd? = null
+    private var mAdIsLoading: Boolean = false
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        MobileAds.initialize(this){}
+        startGame()
         setListeners()
     }
-
     private fun setListeners() {
 
         binding.btnCalcular.setOnClickListener{
-           calcularIMC(
+            calcularIMC(
                binding.pesoEdt.text.toString(),
                binding.alturaEdt.text.toString(),
                binding.idadeEdt.text.toString())
+            showInterstitial()
+
         }
     }
 
@@ -43,6 +51,7 @@ class MainActivity : AppCompatActivity() {
                 binding.idadeEdt.setBackgroundColor(red)
                 binding.idadeEdt.setHint("Preencha o campo idade")
             } else {
+
                 val imc = peso / (altura * altura) //cálculo do imc
                 //val imc = String.format("%.2f",valorImc)
 
@@ -52,6 +61,66 @@ class MainActivity : AppCompatActivity() {
                     intent.putExtra("idade",idade)
                 startActivity(intent)
             }
+    }
+
+    //adMob ---------------------------------------------
+    private fun startGame(){
+        if (!mAdIsLoading && mInterstitialAd == null) {
+            mAdIsLoading = true
+            loadAd()
+        }
+    }
+
+    private fun loadAd(){
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712",
+            adRequest, object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                    mAdIsLoading = false
+/*
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Erro ao carregar anúncio",
+                        Toast.LENGTH_SHORT
+                    ).show()
+*/
+                }
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                    mAdIsLoading = false
+/*
+                    Toast.makeText(this@MainActivity, "onAdLoaded()",
+                        Toast.LENGTH_SHORT).show()
+*/
+                }
+            })
+    }
+
+    private fun showInterstitial() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    mInterstitialAd = null
+                    loadAd()
+                }
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+/*
+                    Toast.makeText(this@MainActivity, "falha ao exibir o anúncio",
+                        Toast.LENGTH_SHORT).show()
+*/
+                    mInterstitialAd = null
+                }
+                override fun onAdShowedFullScreenContent() {
+
+                    // Called when ad is dismissed.
+                }
+            }
+            mInterstitialAd?.show(this)
+        }else {
+//            Toast.makeText(this, "Ad wasn't loaded.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
